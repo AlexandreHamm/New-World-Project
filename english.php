603ddcb320d2e25600137a9f5b7a2fa1b5259c9e
ahamm-prequal2021 <a.hamm@codeur.online>
2021-07-29 16:46:55 +0200

    feat: languages docs

---
+++ english.php
@@ -0,0 +1,43 @@
+<?php
+// NAVBAR
+
+	define('home',"Home");
+	define('guid',"Guides");
+	define('build',"Builds");
+	define('cntct',"Contact");
+
+// GUIDES
+	define('prog',"Progressing");
+		define('chxp',"Character EXP");
+		define('wxp',"Weapons EXP");
+		define('inv',"Invasions");
+
+	define('gather',"Gathering");
+		define('skin',"Skinning");
+		define('min',"Mining");
+		define('log',"Logging");
+		define('harv',"Harvesting");
+		define('fish',"Fishing");
+
+	define('pvp',"PvP");
+		define('war',"Wars");
+		define('fort',"Fortresses");
+		define('city',"Cities");
+
+// BUILDS
+	
+	define('ss',"Epée et bouclier");	
+	define('hatch',"Hachette");
+	define('rap',"Rapière");
+	define('lan',"Lance");
+	define('hamm',"Marteau");
+	define('grtx',"Hache 2 mains");
+	define('bow',"Arc");
+	define('musk',"Fusil");
+	define('fires',"Bâton de feu");
+	define('iceg',"Gantelet de froid");
+	define('lifes',"Bâton de vie");
+
+
+
+?> 
---
+++ french.php
@@ -0,0 +1,50 @@
+<?php
+
+// NAVBAR
+
+	define('home',"Accueil");
+	define('guid',"Guides");
+	define('build',"Builds");
+	define('cntct',"Contact");
+
+// GUIDES
+	define('prog',"Progression");
+		define('chxp',"Exp du personnage");
+		define('wxp',"Exp des armes");
+		define('inv',"Invasions");
+
+	define('gather',"Exploitation");
+		define('skin',"Dépeçage");
+		define('min',"Mineur");
+		define('log',"Bûcheron");
+		define('harv',"Récolte");
+		define('fish',"Pêcheur");
+
+	define('pvp',"PvP");
+		define('war',"Guerres");
+		define('fort',"Forteresses");
+		define('city',"Contrôle de villes");
+
+// BUILDS
+	
+	define('ss',"Epée et bouclier");	
+	define('hatch',"Hachette");
+	define('rap',"Rapière");
+	define('lan',"Lance");
+	define('hamm',"Marteau");
+	define('grtx',"Hache 2 mains");
+	define('bow',"Arc");
+	define('musk',"Fusil");
+	define('fires',"Bâton de feu");
+	define('iceg',"Gantelet de froid");
+	define('lifes',"Bâton de vie");
+
+// 
+
+	define('VARIABLE_2',"Hachette");
+	define('VARIABLE_2',"Hachette");
+	define('VARIABLE_2',"Hachette");
+	define('VARIABLE_2',"Hachette");
+	define('VARIABLE_2',"Hachette");
+	define('VARIABLE_2',"Hachette");
+?> 
