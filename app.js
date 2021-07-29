534a2dca34686edda1100dcb57f715b71d4b3a11
ahamm-prequal2021 <a.hamm@codeur.online>
2021-07-29 09:29:52 +0200

    feat: french nav

---
+++ app.js
@@ -0,0 +1,24 @@
+let contactForm = document.querySelector('.formulaire');
+
+
+function home(){
+    contactForm.style.opacity = '0'
+    contactForm.style.pointerEvents = 'none'
+}
+
+function guides(){
+    contactForm.style.opacity = '0'
+    contactForm.style.pointerEvents = 'none'
+}
+
+function builds(){
+    contactForm.style.opacity = '0'
+    contactForm.style.pointerEvents = 'none'
+}
+
+function contact(){
+    
+
+    contactForm.style.opacity = '100%'
+    contactForm.style.pointerEvents = 'all'
+}
