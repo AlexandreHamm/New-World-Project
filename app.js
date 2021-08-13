f8b9903e7087b1204c180b37b49f184c9226f21c
ahamm-prequal2021 <a.hamm@codeur.online>
2021-08-13 04:03:48 +0200

    fix: new theme js var name

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let contactForm = document.querySelector('.formulaire'),
     confirmation = document.querySelector('.confirmation'),
-    c = 0;
+    cont = 0;
 
 
 function home(){
@@ -27,16 +27,16 @@ function contact(){
 }
 
 function fullscreen(){
-    if(c == 0){
+    if(cont == 0){
         $('.container').css({'position':'absolute','right':'0','transform':'translate(0)','width':'85vw','height':'100vh','margin-top':'0'});
         $('.fullscrnBtn').css({'top':'0','right':'0'});
         $('.bgvid').css('display','none');
-        c = 1;
+        cont = 1;
     }
-    else if(c == 1){
+    else if(cont == 1){
         $('.container').css({'position':'relative','right':'-50%','transform':'translateX(-50%)','width':'70vw','height':'100vh','margin-top':'2rem'});
         $('.fullscrnBtn').css({'top':'20vh','right':'13.7vw'});
         $('.bgvid').css('display','block');
-        c = 0;
+        cont = 0;
     }
 }
