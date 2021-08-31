ce3b28e9736e29c903f294a30636ecfecda2e725
ahamm-prequal2021 <a.hamm@codeur.online>
2021-08-31 11:02:28 +0200

    changed description

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Company site for Cave Canem on Amazon's game 'New World'">
+    <meta name="description" content="Website for company Ad Gloriam on Amazon's game 'New World'">
     <meta name="keywords" content="Ad, Gloriam, Ad Gloriam, New, World, New World, Amazon, Game, Company">
     <title>Ad Gloriam</title>
     <link rel="stylesheet" href="./style.css">
