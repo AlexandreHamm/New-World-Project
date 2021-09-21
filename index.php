6fed3e10498a84e54af1ae72ba279a204ecf0743
ahamm-prequal2021 <a.hamm@codeur.online>
2021-09-21 16:01:57 +0200

    htaccess

--- index.php
+++ index.php
@@ -1,120 +1,120 @@
 <?php include 'header.php'?>
-            <div class="white">
-                <h3>Latest</h3><br>
-                <div class="cardWrapper">
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
+    <div class="white">
+        <h3>Latest</h3><br>
+        <div class="cardWrapper">
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
                 </div>
-                <div class="cardWrapper">
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="cardImg">
-                            <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
-                        </div>
-                        <div class="cardContent">
-                            <h5>Lorem Ipsum</h5>
-                            <p class='dates'>21 Septembre 2021</p>
-                            <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
-                        </div>
-                    </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
                 </div>
-                <!-- <div class="cardWrapper">
-                    <div class="card">
-
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                </div>
-                <div class="cardWrapper">
-                    <div class="card">
-
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                    <div class="card">
-                        
-                    </div>
-                </div> -->
-                
-                <h6>k.n. aries</h6>
-                <!-- <p class='credits' style='text-align:center'>K.N. © ARIES 2021</p> -->
             </div>
-        <?php include 'footer.php'?>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+        </div>
+        <div class="cardWrapper">
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="cardImg">
+                    <img src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png" alt="">
+                </div>
+                <div class="cardContent">
+                    <h5>Lorem Ipsum</h5>
+                    <p class='dates'>21 Septembre 2021</p>
+                    <p class='intro'>Mais où est ta virginité, elle n'est pas sur la carte. T'es obligé de prendre un Khajiit pour jouer avec une chatte.</p>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="cardWrapper">
+            <div class="card">
+
+            </div>
+            <div class="card">
+                
+            </div>
+            <div class="card">
+                
+            </div>
+            <div class="card">
+                
+            </div>
+        </div>
+        <div class="cardWrapper">
+            <div class="card">
+
+            </div>
+            <div class="card">
+                
+            </div>
+            <div class="card">
+                
+            </div>
+            <div class="card">
+                
+            </div>
+        </div> -->
+        
+        <h6>k.n. aries</h6>
+        <!-- <p class='credits' style='text-align:center'>K.N. © ARIES 2021</p> -->
+    </div>
+<?php include 'footer.php'?>
